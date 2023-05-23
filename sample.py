ca659f2395a8f487cf8fe6283f48a2f3a603df6f
valentinanaldi99 <79771357+valentinanaldi99@users.noreply.github.com>
2023-05-23 14:00:26 +0200

    changed sample.py

--- sample.py
+++ sample.py
@@ -7,15 +7,17 @@ from process_image import process_image
 app = Flask(__name__)
 
 def validateConfiguration(data):
-    """ This function is designed to validate the configuration dictionary by checking 
-        the presence of mandatory fields, the correctness of date formats, the order of dates,
-        and the validity of the number of results if provided. If any of the validation 
-        checks fail, a ValueError is raised with an appropriate error message.
-    """
-    mandatory_fields = ['keywords','start_date','final_date']   # TODO are just these?
+    mandatory_fields = ['type', 'images']
+    mandatory_fields_image = ['URLImage', 'date']
     if any(x not in list(data.keys()) for x in mandatory_fields):
         raise ValueError("One or more mandatory field(s) is missing")
     s = f = None
+
+    images = data['images']
+    for image in images:
+        print(image.keys())
+        if any(x not in list(image.keys()) for x in mandatory_fields_image):
+            raise ValueError("One or more mandatory field(s) is missing from image")
     try:
         s = datetime.strptime(data['start_date'], "%Y-%m-%d")
         f = datetime.strptime(data['final_date'], "%Y-%m-%d")
@@ -28,33 +30,17 @@ def validateConfiguration(data):
 
 @app.route('/group3output/')
 def serve_files():
-    """ The purpose of this code is to generate an HTML response that displays the list 
-        of files in the output_folder. Each file is shown as a clickable link, allowing 
-        users to access individual files by clicking on the corresponding link.
-    """
     file_dir = os.getcwd()+"/files/output_folder"
     files = os.listdir(file_dir)
-    response = ""
+    elements = []
     for filename in files:
-        response += f"<a href='/group3output/{filename}'>{filename}</a><br>"
-    return response
-
-@app.route('/group3output/<path:filename>')
-def serve_file(filename):
-    """ The purpose of this code is to handle requests for specific files in the output_folder. 
-        When a request is made to /group3output/<filename>, the corresponding file is located in 
-        the output_folder directory, and Flask sends it back to the client as a response.
-    """
-    return send_from_directory(os.getcwd()+"/files/output_folder", filename)
+        f = open(file_dir + '/' + filename)
+        contents = json.load(f)
+        elements.append(contents)
+    return json.dumps(elements)
 
 @app.route('/processing/start', methods=['POST'])
 def start_processing():
-    """ The purpose of this code is to handle POST requests to '/processing/start' that contain 
-        uploaded files. It reads the contents of each JSON file, parses the JSON data, and passes 
-        it to the process_image() function for further processing. If any of the uploaded files do 
-        not contain valid JSON, a ValueError is raised. Finally, the route returns "done" as the 
-        response to indicate that the processing is complete.
-    """
     json_files = request.files.getlist('files')
     # 'files' is the name of the file input field in the form
 
@@ -66,16 +52,10 @@ def start_processing():
         # Parse the JSON data
         try:
             json_payload = json.loads(json_data)
+            #validateConfiguration(json_payload)
             process_image(json_payload)
         except ValueError:
             raise ValueError('Invalid JSON format')
         # Continue processing the JSON payload as needed
 
     return "done"
-
-if __name__ == '__sample__':
-    # to run on server
-    app.run(host='3.69.174.135', port=3333)
-
-    # to run on localhost
-    #app.run(host='localhost', port=5000)
--- sender.py
+++ sender.py
@@ -8,7 +8,7 @@ import json
 
 # Sample JSON payloads
 json_payloads = [
-{"id": "gr1-Syria-2023_02_06T00_00_00-2023_02_06T12_00_00-earthquake", "country": "Syria", "date": "2023-02-06 01:36:27", "type": "earthquake", "timeframe": ["2023-02-06 00:00:00", "2023-02-06 12:00:00"], "locations": [[36.6832, 36.9921], [36.6208, 37.0302], [36.7588, 38.0752]], "images": [{"URLTweet": "https://twitter.com/HumanDilemma_/status/1622560743265648642", "URLImage": "https://pbs.twimg.com/ext_tw_video_thumb/1622559844782727168/pu/img/IcvoAEOS_PVspCQX.jpg", "date": "2023-02-06 11:40:01"}, {"URLTweet": "https://twitter.com/AzadMirzaa/status/1622565757472088072", "URLImage": "https://pbs.twimg.com/media/FoSC8_qWAAExPhW?format=jpg&name=900x900", "date": "2023-02-06 11:59:57"}]}
+{"id": "gr1-Syria-2023_02_06T00_00_00-2023_02_06T12_00_00-earthquake", "country": "Syria", "date": "2023-02-06 01:36:27", "type": "earthquake", "timeframe": ["2023-02-06 00:00:00", "2023-02-06 12:00:00"], "locations": [[36.6832, 36.9921], [36.6208, 37.0302], [36.7588, 38.0752]], "images": [{"tweet_url": "https://twitter.com/HumanDilemma_/status/1622560743265648642", "URLImage": "https://pbs.twimg.com/ext_tw_video_thumb/1622559844782727168/pu/img/IcvoAEOS_PVspCQX.jpg", "date": "2023-02-06 11:40:01"}, {"URLTweet": "https://twitter.com/AzadMirzaa/status/1622565757472088072", "URLImage": "https://pbs.twimg.com/media/FoSC8_qWAAExPhW?format=jpg&name=900x900", "date": "2023-02-06 11:59:57"}]}
 ]
 
 # Create a list of file objects with the JSON data
@@ -22,6 +22,6 @@ response = requests.post('http://3.69.174.135:3333/processing/start', files=file
 
 # Check the response
 if response.status_code == 200:
-    print('ok')
+    print(response)
 else:
     print(f"Error: {response.status_code} - {response.text}")
